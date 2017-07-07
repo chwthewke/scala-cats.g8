5330303f9464fa5edb9b955fb328757b8169cede
Chewie <chwthewke@gmail.com>
2017-07-07 19:45:29 +0200

    feat(scala-cats.g8): Update default & dependency versions

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,22 +6,22 @@ object Dependencies {
     artifacts.map(organization       %% _ % version withSources ()) ++
       testArtifacts.map(organization %% _ % version % "test" withSources ())
 
-  val kindProjector = compilerPlugin("org.spire-math" %% "kind-projector" % "0.9.3")
+  val kindProjector = compilerPlugin("org.spire-math" %% "kind-projector" % "0.9.4")
 
-  val catsVersion = "0.8.1"
+  val catsVersion = "0.9.0"
 
   val cats = depsGroup("org.typelevel", catsVersion)("cats-core")("cats")
 
-  val mouse = ("com.github.benhutchison" %% "mouse" % "0.6").withSources().exclude("org.typelevel", "cats")
+  val mouse = ("com.github.benhutchison" %% "mouse" % "0.9").withSources().exclude("org.typelevel", "cats")
 
   val catsAll = cats ++ Seq(mouse)
 
   val scalaArm = "com.jsuereth" %% "scala-arm" % "2.0" withSources ()
 
-  val scalatest = "org.scalatest" %% "scalatest" % "3.0.1" withSources ()
+  val scalatest = "org.scalatest" %% "scalatest" % "3.0.3" withSources ()
 
   val scalacheck =
-    Seq("org.scalacheck" %% "scalacheck" % "1.13.4", "io.github.amrhassan" %% "scalacheck-cats" % "0.3.2")
+    Seq("org.scalacheck" %% "scalacheck" % "1.13.5", "io.github.amrhassan" %% "scalacheck-cats" % "0.3.3")
       .map(_.withSources())
 
   val monocleVersion = "1.4.0"
@@ -29,7 +29,7 @@ object Dependencies {
   val monocle =
     depsGroup("com.github.julien-truffaut", monocleVersion)("monocle-core", "monocle-macro")()
 
-  val circeVersion = "0.6.1"
+  val circeVersion = "0.8.0"
 
   val circe =
     depsGroup("io.circe", circeVersion)("circe-core", "circe-generic", "circe-optics", "circe-parser")()
--- src/main/g8/project/plugins.sbt
+++ src/main/g8/project/plugins.sbt
@@ -10,10 +10,10 @@ resolvers += Resolver.sonatypeRepo("snapshots")
 
 addSbtPlugin("org.scoverage" %% "sbt-scoverage" % "1.5.0")
 
-addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.6.1")
+addSbtPlugin("com.eed3si9n" % "sbt-buildinfo" % "0.7.0")
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.1.4")
+addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.2.0")
 
-addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.3")
+addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.5")
 
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.0")
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.3.1")
